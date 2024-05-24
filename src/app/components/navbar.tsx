ede93f553196bf7296d0791ec16002a305d8f5dd
kareem shousha <kemoshousha309@gmail.com>
2024-05-24 19:30:41 +0300

    Add prisma and issue model

--- src/app/components/navbar.tsx
+++ src/app/components/navbar.tsx
@@ -1,11 +1,16 @@
+"use client";
+import classNames from "classnames";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { FaBug } from "react-icons/fa";
 
-const Navbar = async () => {
+const Navbar = () => {
   const links = [
     { label: "Dashboard", href: "/" },
     { label: "Issues", href: "/issues" },
   ];
+  const path = usePathname();
+
   return (
     <nav className="flex space-x-6 border-b h-14 px-4 items-center ">
       <Link href="/">
@@ -14,7 +19,15 @@ const Navbar = async () => {
       <ul className="flex space-x-6">
         {links.map((l) => (
           <li key={l.label}>
-            <Link className="text-zinc-400 hover:text-zinc-800 transition-colors" href={`${l.href}`}>{l.label}</Link>
+            <Link
+              className={classNames(" hover:text-zinc-800 transition-colors", {
+                "text-zinc-900": path === l.href,
+                "text-zinc-400": path !== l.href,
+              })}
+              href={`${l.href}`}
+            >
+              {l.label}
+            </Link>
           </li>
         ))}
       </ul>
---
+++ src/app/issues/page.tsx
@@ -0,0 +1,11 @@
+
+const ProfilePage =  () => {
+
+  return (
+    <div className="">
+      <h1>Welcome to the Issues page</h1>
+    </div>
+  );
+};
+
+export default ProfilePage;

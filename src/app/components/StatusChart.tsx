6fc69e28e5c1818024b8d96cc12270cc23a78fc1
kareem shousha <kemoshousha309@gmail.com>
2024-06-09 02:50:26 +0300

    sentry setup

--- src/app/components/StatusChart.tsx
+++ src/app/components/StatusChart.tsx
@@ -35,6 +35,9 @@ const StatusChart = ({closed, inProgress ,open}: Props) => {
   ];
   return (
     <Card className="h-80">
+      <button onClick={() => {
+        throw new Error("sentry error")
+      }} >click</button>
       <ResponsiveContainer width="100%" height="100%">
         <BarChart data={data}>
           <CartesianGrid strokeDasharray="3 3" />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,28 @@
 import { revalidatePath } from 'next/cache';
 import LatestIssues from "./components/LatestIssues";
+import IssuesSummary from './components/IssuesSummary';
+import prisma from '../../prisma/client';
+import StatusChart from './components/StatusChart';
+import { Flex, Grid } from '@radix-ui/themes';
+import { Metadata } from 'next';
 export default async function Home() {
+  const open = await prisma.issue.count({where: {status: "OPEN"}})
+  const inProgress = await prisma.issue.count({where: {status: "IN_PROGRESS"}})
+  const closed = await prisma.issue.count({where: {status: "CLOSED"}})
+  const statues = {open, inProgress, closed}
   return (
-    <div className="home">
+    <Grid columns={{initial: "1", md: "2"}} gap="5" >
+      <Flex direction="column" gap="4">
+      <IssuesSummary {...statues} />
+      <StatusChart {...statues} />
+      </Flex>
       <LatestIssues />
-    </div>
+    </Grid>
   );
 }
 revalidatePath('/')
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Dashboard",
+  description: "View a summary of the project issues"
+}
